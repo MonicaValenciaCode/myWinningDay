16efebaf721b297bed51ca564fe54e4a9f0aafb9
MonicaValenciaCode <monicacoding@outlook.com>
2025-01-22 22:53:53 +0000

    test coverage

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,16 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   testEnvironment: "jest-environment-jsdom", // Explicitly specify the environment
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], // Jest setup file
+  collectCoverage: true, // Enable coverage collection
+  coverageThreshold: {
+    // Set coverage thresholds
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
